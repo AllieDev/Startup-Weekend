11d7dba3612bb2daabe3de59c393cc88fb2cefe6
Ali Sultani <sultanialiasghar@gmail.com>
2022-09-25 15:25:25 +1300

    USER DATA CHANGED

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -1,116 +1,120 @@
 export const selectedProducts = {
-  name: 'Zip Tote Basket',
-  price: 'budget $20',
+  name: "Zip Tote Basket",
+  price: "budget $20",
   rating: 4,
   images: [
     {
       id: 1,
-      name: 'Angled view',
-      src: 'https://storage.re-mint.com/longwaycalifornia/listings/10010389001-e7fdc2f7f651cb50dc34a091a943aa8e.jpeg',
-      alt: 'Angled front view with bag zipped and handles upright.'
-    }
+      name: "Angled view",
+      src: "https://storage.re-mint.com/longwaycalifornia/listings/10010389001-e7fdc2f7f651cb50dc34a091a943aa8e.jpeg",
+      alt: "Angled front view with bag zipped and handles upright.",
+    },
     // More images...
   ],
-  categories: ['Jackets'],
+  categories: ["Jackets"],
   colors: [
     {
-      name: 'Washed Black',
-      bgColor: 'bg-gray-700',
-      selectedColor: 'ring-gray-700'
+      name: "Washed Black",
+      bgColor: "bg-gray-700",
+      selectedColor: "ring-gray-700",
     },
     {
-      name: 'White',
-      bgColor: 'bg-white',
-      selectedColor: 'ring-gray-400'
+      name: "White",
+      bgColor: "bg-white",
+      selectedColor: "ring-gray-400",
     },
     {
-      name: 'Washed Gray',
-      bgColor: 'bg-gray-500',
-      selectedColor: 'ring-gray-500'
-    }
+      name: "Washed Gray",
+      bgColor: "bg-gray-500",
+      selectedColor: "ring-gray-500",
+    },
   ],
   description: `
         <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
       `,
   details: [
     {
-      name: 'Features',
+      name: "Features",
       items: [
-        'Multiple strap configurations',
-        'Spacious interior with top zip',
-        'Leather handle and tabs',
-        'Interior dividers',
-        'Stainless strap loops',
-        'Double stitched construction',
-        'Water-resistant'
-      ]
-    }
+        "Multiple strap configurations",
+        "Spacious interior with top zip",
+        "Leather handle and tabs",
+        "Interior dividers",
+        "Stainless strap loops",
+        "Double stitched construction",
+        "Water-resistant",
+      ],
+    },
     // More sections...
-  ]
-}
+  ],
+};
 
 export const users = [
   {
-    role: 'brand',
-    name: 'Helestines',
-    profileImg: '../../assets'
+    role: "brand",
+    name: "Helestines",
+    profileImg: "../../assets",
   },
   {
-    role: 'contractor',
-    name: 'John Doe',
-    profileImg: '../assets/unsplash_IF9TK5Uy-KI.png'
+    role: "contractor",
+    name: "John Doe",
+    profileImg: "../assets/unsplash_IF9TK5Uy-KI.png",
   },
   {
-    role: 'customer',
-    name: 'Sara Tay',
-    profileImg: '../../assets/unsplash_rDEOVtE7vOs.png'
-  }
-]
+    role: "customer",
+    name: "Sara Tay",
+    profileImg: "../../assets/unsplash_rDEOVtE7vOs.png",
+  },
+];
 
 export const contractors = [
   {
     id: 1,
-    name: 'Ben',
+    name: "Ben Lee",
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been",
     rating: 5,
     reviewCount: 38,
-    imageSrc: 'https://www.w3schools.com/w3images/avatar2.png',
-    imageAlt: 'TODO',
-    href: '#'
+    imageSrc:
+      "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?crop=entropy&cs=tinysrgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDN8fHByb2ZpbGV8ZW58MHx8fHwxNjY0MDU0MDk1&ixlib=rb-1.2.1&q=80&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450",
+    imageAlt: "TODO",
+    href: "#",
   },
   {
     id: 2,
-    name: 'Adam Wild',
+    name: "Kate Wild",
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been",
     rating: 5,
     reviewCount: 18,
-    imageSrc: 'https://www.w3schools.com/w3images/avatar2.png',
-    imageAlt: 'TODO',
-    href: '#'
+    imageSrc:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?crop=entropy&cs=tinysrgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDE1fHxwcm9maWxlfGVufDB8fHx8MTY2NDA1NDA5NQ&ixlib=rb-1.2.1&q=80&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450",
+    imageAlt: "TODO",
+    href: "#",
   },
   {
     id: 3,
-    name: 'Dave Smith',
+    name: "Sara Smith",
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been",
     rating: 5,
     reviewCount: 14,
-    imageSrc: 'https://www.w3schools.com/w3images/avatar2.png',
-    imageAlt: 'TODO',
-    href: '#'
+    imageSrc:
+      "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?crop=entropy&cs=tinysrgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDEyfHxwcm9maWxlfGVufDB8MHx8fDE2NjQwNTk1MzA&ixlib=rb-1.2.1&q=80&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450",
+    imageAlt: "TODO",
+    href: "#",
   },
   {
     id: 4,
-    name: 'Anusha Dira',
+    name: "Anusha Dira",
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been",
     rating: 4,
     reviewCount: 21,
-    imageSrc: 'https://www.w3schools.com/w3images/avatar2.png',
-    imageAlt: 'TODO',
-    href: '#'
-  }
+    imageSrc:
+      "https://images.unsplash.com/photo-1474978528675-4a50a4508dc3?crop=entropy&cs=tinysrgb&fm=jpg&ixid=Mnw3MjAxN3wwfDF8c2VhcmNofDl8fHByb2ZpbGV8ZW58MHwwfHx8MTY2NDA1OTUzMA&ixlib=rb-1.2.1&q=80&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450",
+    imageAlt: "TODO",
+    href: "#",
+  },
   // More products...
-]
+];
